50bfc923cecd44b4be67a144dec70213fd971a24
wessouzza <wes-souza@hotmail.com>
2023-10-12 17:09:18 -0300

    Correction: Table name

---
+++ src/main/java/com/stock_mgmt/stock_mgmt/entity/UserRole.java
@@ -0,0 +1,50 @@
+package com.stock_mgmt.stock_mgmt.entity;
+
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.stock_mgmt.stock_mgmt.enums.RoleName;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_roles")
+public class UserRole implements GrantedAuthority {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+    
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+
+    public UUID getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(UUID roleId) {
+        this.roleId = roleId;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+}
