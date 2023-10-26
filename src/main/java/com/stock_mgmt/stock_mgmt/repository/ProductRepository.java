b1ee5f23f7115b44e5762617238a02b19e1f9c64
wessouzza <wes-souza@hotmail.com>
2023-10-26 15:11:25 -0300

    Fix: Product repository

--- src/main/java/com/stock_mgmt/stock_mgmt/repository/ProductRepository.java
+++ src/main/java/com/stock_mgmt/stock_mgmt/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package com.stock_mgmt.stock_mgmt.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.stock_mgmt.stock_mgmt.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
-    
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findBySku(Long sku);
+    List<Product> findByName(String name);
 }
